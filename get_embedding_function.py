68def7cf1d6e56b1c3a36cef5e686c1ae93a25c8
vitoria.assumpcao <vitoria-assumpcao@hotmail.com>
2025-08-15 09:12:05 -0300

    Atualiza imports e configura Ollama

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -3,4 +3,4 @@ from langchain_ollama import OllamaEmbeddings
 #return for use of the local ollama with the llama3.1 model
 def get_embedding_function():
     
-    return OllamaEmbeddings(model="llama3.1")
+    return OllamaEmbeddings(model="nomic-embed-text")
